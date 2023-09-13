691f1105fc491296c1880076e1c97d9e36a67ec9
Your Name <you@example.com>
2023-09-13 07:51:53 -0300

    refatorando

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
-// Importe as bibliotecas do Firebase
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+import 'package:flutter/material.dart';
+import 'package:tdah_app/telas/Home.dart'; // Importar o pacote de gráficos
 
-// Configure o Firebase com as informações do seu projeto
-const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID',
-};
-
-// Inicialize o Firebase
-firebase.initializeApp(firebaseConfig);
+void main() {
+  runApp(const HomeAppTDAH());
+}
