cc38a5967984abff12edcd981d923c9eaea860fe
Your Name <you@example.com>
2023-09-13 21:15:09 -0300

    atualizando 13/09

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:tdah_app/telas/Home.dart'; // Importar o pacote de gráficos
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const HomeAppTDAH());
 }
